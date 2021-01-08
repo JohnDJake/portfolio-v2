2529a7fafb196d08b55f7865b2a5a1b7e7915c73
JohnDJake <john.d.jake@gmail.com>
2021-01-07 22:55:26 -0800

    fix mobile sidebar behavior

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
     // Initialize the mobile sidenav
-    $('.sidenav').sidenav();
+    $(".sidenav").sidenav();
+
+    // Add event handler to close the mobile sidenav when one of the links is clicked
+    $(".sidenav a").click(() => $(".sidenav-overlay").click());
   });
